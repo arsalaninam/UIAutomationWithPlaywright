125d527e273ce5927a0cf11106c9dbbc54fdc1d7
Arsalan Inam <m.arsalaninam@gmail.com>
2023-03-31 06:54:32 +0200

    Add ui selectors for Italian restaurants testcase

--- pages/listingpage.po.js
+++ pages/listingpage.po.js
@@ -1,6 +1,9 @@
 module.exports = {
 
     radioButton10EuroOrLess: '//fieldset[@data-qa="minimum-order-value-filter-options"]//div[2]//span',
-    listOfRestaurants: '//div[contains(@class, "_3lXnmO")]//section//li'
+    listOfRestaurants: '//div[contains(@class, "_3lXnmO")]//section//li',
+    footer: '//a[contains(@data-qa, "footer-link-privacyStatement")]',
+    italianCategoryButtonSelector: '//div[text()="Italian"]',
+    restaurantSidebarResultCount: '//h3[@data-qa="sidebar-result-counter"]'
   };
   
